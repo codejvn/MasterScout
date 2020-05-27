ad50840d3c81239dd0600d6ea014d51d0f3243e0
Rohan Bhatnagar <rohan.bha09@gmail.com>
2020-05-27 15:46:54 -0400

    charts work with raw data

---
+++ src/Components/Layout/ManageDataTabs/AutoChart.js
@@ -0,0 +1,54 @@
+import React, { Component } from "react";
+import { Bar } from "react-chartjs-2";
+
+export class AutoChart extends Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            data: {
+                labels: ["AUTO INNER", "AUTO OUTER", "AUTO BOTTOM", "AUTO MISSED", "AUTO ATTEMPTED"],
+                datasets: [
+                    {
+                        data: [4, 5, 3, 5, 2, 3],
+                        label: "POWER CELLS SCORED IN AUTONOMOUS",
+                        backgroundColor: [
+                            'rgba(255, 99, 132, 0.2)',
+                            'rgba(54, 162, 235, 0.2)',
+                            'rgba(255, 206, 86, 0.2)',
+                            'rgba(75, 192, 192, 0.2)',
+                            'rgba(153, 102, 255, 0.2)',
+                            'rgba(255, 159, 64, 0.2)'
+                        ],
+                        borderColor: [
+                            'rgba(255, 99, 132, 1)',
+                            'rgba(54, 162, 235, 1)',
+                            'rgba(255, 206, 86, 1)',
+                            'rgba(75, 192, 192, 1)',
+                            'rgba(153, 102, 255, 1)',
+                            'rgba(255, 159, 64, 1)'
+                        ],
+                        borderWidth: 1
+
+                    }
+                ]
+            }
+        }
+    }
+    render() {
+        return (
+            <div style={{ position: "relatuve", width: "400", height: "200" }}>
+                <Bar
+                    options={{
+                        responsive: true
+                    }}
+                    data={this.state.data}
+                />
+            </div>
+        )
+    }
+}
+const spacer = {
+    padding: "2vh",
+};
+
+export default AutoChart;
--- src/Components/Layout/ManageDataTabs/TeamBreakdown.js
+++ src/Components/Layout/ManageDataTabs/TeamBreakdown.js
@@ -1,80 +1,79 @@
-
-import { Bar } from "react-chartjs-2";
+import React, { Component } from "react";
+import Accordion from "react-bootstrap/Accordion";
+import Card from "react-bootstrap/Card";
 import { connect } from "react-redux";
+import Container from "react-bootstrap/Container";
+import AutoChart from "./AutoChart.js";
+import TeleopChart from "./TeleopChart.js";
+import EndgameChart from "./EndgameChart.js";
 
 export class TeamBreakdownRaw extends Component {
-  render() {
-    let searchedTeam = this.props.search.teamSearched; // this is the boy
-    return (
-      <Container style={containerWidth}>
-        {/* AUTONOMOUS */}
-        <Accordion style={spacer} defaultActiveKey="1">
-          <Card>
-            <Accordion.Toggle as={Card.Header} eventKey="0">
-              AUTONOMOUS
+    render() {
+        let searchedTeam = this.props.search.teamSearched; // this is the boy
+        return (
+            <Container style={containerWidth}>
+                {/* AUTONOMOUS */}
+                <Accordion style={spacer} defaultActiveKey="1">
+                    <Card>
+                        <Accordion.Toggle as={Card.Header} eventKey="0">
+                            AUTONOMOUS
             </Accordion.Toggle>
-            <Accordion.Collapse eventKey="0">
-              <div>
-                <canvas style={chart}></canvas>
-              </div>
-            </Accordion.Collapse>
-          </Card>
-        </Accordion>
+                        <Accordion.Collapse eventKey="0">
+                            <div><AutoChart /></div>
+                        </Accordion.Collapse>
+                    </Card>
+                </Accordion>
 
-        {/* TELEOP */}
-        <Accordion style={spacer} defaultActiveKey="1">
-          <Card>
-            <Accordion.Toggle as={Card.Header} eventKey="0">
-              TELEOPERATED
+                {/* TELEOP */}
+                <Accordion style={spacer} defaultActiveKey="1">
+                    <Card>
+                        <Accordion.Toggle as={Card.Header} eventKey="0">
+                            TELEOPERATED
             </Accordion.Toggle>
-            <Accordion.Collapse eventKey="0">
-              <div>
-                <canvas style={chart}></canvas>
-              </div>
-            </Accordion.Collapse>
-          </Card>
-        </Accordion>
+                        <Accordion.Collapse eventKey="0">
+                            <div><TeleopChart /></div>
+                        </Accordion.Collapse>
+                    </Card>
+                </Accordion>
 
-        {/* ENDGAME */}
-        <Accordion style={spacer} defaultActiveKey="1">
-          <Card>
-            <Accordion.Toggle as={Card.Header} eventKey="0">
-              ENDGAME
+                {/* ENDGAME */}
+                <Accordion style={spacer} defaultActiveKey="1">
+                    <Card>
+                        <Accordion.Toggle as={Card.Header} eventKey="0">
+                            ENDGAME
             </Accordion.Toggle>
-            <Accordion.Collapse eventKey="0">
-              <div>
-                <canvas style={chart}></canvas>
-              </div>
-            </Accordion.Collapse>
-          </Card>
-        </Accordion>
-      </Container>
-    );
-  }
+                        <Accordion.Collapse eventKey="0">
+                            <div><EndgameChart /></div>
+                        </Accordion.Collapse>
+                    </Card>
+                </Accordion>
+            </Container>
+        );
+    }
 }
 const chart = {
-  width: "400",
-  height: "200",
-  backgroundColor: "red",
+    width: "400",
+    height: "200",
+    backgroundColor: "red",
 };
 const spacer = {
-  padding: "0.5vh",
+    padding: "0.5vh",
 };
 const containerWidth = {
-  width: "100%",
+    width: "100%",
 };
 const mapStateToProps = (state) => {
-  return {
-    search: state.search,
-  };
+    return {
+        search: state.search,
+    };
 };
 const mapDispatchToProps = (dispatch) => {
-  // propName: (parameters) => dispatch(action)
-  return {
-    // put actions here
-  };
+    // propName: (parameters) => dispatch(action)
+    return {
+        // put actions here
+    };
 };
 export const TeamBreakdown = connect(
-  mapStateToProps,
-  mapDispatchToProps
+    mapStateToProps,
+    mapDispatchToProps
 )(TeamBreakdownRaw);
